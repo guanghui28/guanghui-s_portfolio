d4c91f6518188b82354ab5fa300b02ba84ebb54a
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-10 09:44:22 +0700

    set up some images and background

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${roboto.className} bg-gray-50 text-gray-950`}>
+			<body className={`${roboto.className} bg-gray-50 text-gray-950 relative`}>
+				<div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] w-[31.5rem] h-[31.5rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
+				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] w-[50rem] h-[31.5rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left[-5rem]" />
 				{children}
 			</body>
 		</html>
